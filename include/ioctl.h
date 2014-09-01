3a1f782d05ea89624980b50598c6e1d551cf823f
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2014-09-01 16:52:19 +0900

    add ioctl.h and modify code.

---
+++ include/ioctl.h
@@ -0,0 +1,138 @@
+/**
+ * ioctl.h - data structure definictions for bdevt ioctl interface.
+ */
+#ifndef BDEVT_IOCTL_H
+#define BDEVT_IOCTL_H
+
+#ifdef __KERNEL__
+#include <linux/kernel.h>
+#include <linux/ioctl.h>
+#else /* __KERNEL__ */
+#include <stdio.h>
+#include <sys/ioctl.h>
+#endif /* __KERNEL__ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * If you want to assign device minor automatically, specify this.
+ */
+#define BDEVT_DYNAMIC_MINOR (-1U)
+
+struct bdevt_ctl {
+	/* Command id. */
+	int command;
+
+	/* Used for integer value transfer. */
+	union {
+		int val_int;
+		u64 val_u64;
+		u32 val_u32;
+	};
+} __attribute__((packed));
+
+/**
+ * Ioctl command id/cmd.
+ */
+#define BDEVT_IOCTL_ID 0xfe
+#define BDEVT_IOCTL_CMD 3
+
+#define BDEVT_IOCTL _IOWR(BDEVT_IOCTL_ID, BDEVT_IOCTL_CMD, struct bdevt_ctl)
+
+/**
+ * For bdevt_ctl.command.
+ */
+enum {
+	BDEVT_IOCTL_DUMMY = 0,
+
+	/****************************************
+	 * The target is /dev/bdevt_ctl
+	 ****************************************/
+
+	/*
+	 * Start a bdevt device.
+	 *
+	 * INPUT: ctl->val_u64 as device size [logical block].
+	 * OUTPUT: ctl->val_u32 as minor id.
+	 * RETURN: 0 in success, or -EFAULT.
+	 */
+	BDEVT_IOCTL_START_DEV,
+
+	/*
+	 * Get major id.
+	 *
+	 * INPUT: None.
+	 * OUTPUT: ctl->val_u32 as major id.
+	 * RETURN: 0 in success, or -EFAULT.
+	 */
+	BDEVT_IOCTL_GET_MAJOR,
+
+	/*
+	 * Get numbr of bdevt devices.
+	 *
+	 * INPUT: None.
+	 * OUTPUT: ctl->val_int as number of bdevt devices.
+	 * RETURN: 0 in success, or -EFAULT.
+	 */
+	BDEVT_IOCTL_NUM_OF_DEV,
+
+	/****************************************
+	 * The targets are bdevt devices.
+	 ****************************************/
+
+	/*
+	 * Stop a bdevt device.
+	 *
+	 * INPUT: None.
+	 * OUTPUT: None.
+	 * RETURN: 0 in success, or -EFAULT.
+	 */
+	BDEVT_IOCTL_STOP_DEV,
+
+	/*
+	 * Make the device crash.
+	 *
+	 * INPUT: None.
+	 * OUTPUT: None.
+	 * RETURN: 0 in success, or -EFAULT.
+	 */
+	BDEVT_IOCTL_MAKE_CRASH,
+
+	/*
+	 * Recover the device from crash.
+	 *
+	 * INPUT: None.
+	 * OUTPUT: None.
+	 * RETURN: 0 in success, or -EFAULT.
+	 */
+	BDEVT_IOCTL_RECOVER_CRASH,
+
+	/*
+	 * Make the device the state that IOs will fail.
+	 *
+	 * INPUT: ctl->val_int
+	 *   0: read will fail.
+	 *   1: write will fail.
+	 *   2: both will fail.
+	 * OUTPUT: None.
+	 * RETURN: 0 in success, or -EFAULT.
+	 */
+	BDEVT_IOCTL_MAKE_ERROR,
+
+	/*
+	 * Recover the device from error.
+	 *
+	 * INPUT: None.
+	 * OUTPUT: None.
+	 * RETURN: 0 in success, or -EFAULT.
+	 */
+	BDEVT_IOCTL_RECOVER_ERROR,
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BDEVT_IOCTL_H */
--- module/bdevt.c
+++ module/bdevt.c
@@ -11,18 +11,22 @@
 #include <linux/bio.h>
 #include <linux/workqueue.h>
 #include <linux/string.h>
+#include <linux/slab.h>
+#include <linux/compat.h>
+#include <linux/miscdevice.h>
 
 #include "common.h"
 #include "block_size.h"
 #include "logger.h"
 #include "size_list.h"
 #include "build_date.h"
+#include "ioctl.h"
 
 /*******************************************************************************
  * Struct definition.
  *******************************************************************************/
 
-struct smpl_dev
+struct bdevt_dev
 {
 	struct list_head list;
 	u32 index;
@@ -36,15 +40,10 @@ struct smpl_dev
 
 static int major_;
 static int pbs_ = 512;
-static int nr_dev_ = 1;
-/* specify 1m,2m ot create two device with 1MiB, 2MiB sizes */
-static char *dev_sizes_ = "1m";
 
 static LIST_HEAD(dev_list_);
 static struct mutex lock_;
 static int dev_indexes_ = 0;
-static int hw_queue_depth_ = 64;
-static int nr_submit_queues_ = 1;
 
 /*******************************************************************************
  * Module parameter definitions.
@@ -52,20 +51,13 @@ static int nr_submit_queues_ = 1;
 
 module_param_named(pbs, pbs_, int, S_IRUGO);
 MODULE_PARM_DESC(pbs, "Physical block size [byte]");
-module_param_named(nr_dev, nr_dev_, int, S_IRUGO);
-MODULE_PARM_DESC(nr_dev, "Number of devices");
-module_param_named(hw_queue_depth, hw_queue_depth_, int, S_IRUGO);
-MODULE_PARM_DESC(hw_queue_depth, "Hardware queue depth.");
-module_param_named(nr_submit_queues, nr_submit_queues_, int, S_IRUGO);
-MODULE_PARM_DESC(nr_submit_queues, "Number of submit queues.");
-module_param_named(dev_sizes, dev_sizes_, charp, S_IRUGO);
-MODULE_PARM_DESC(dev_sizes, "Device size lists separated by comma. ex. '1m,2m'");
 
 /*******************************************************************************
  * Macro definitions.
  *******************************************************************************/
 
-#define SMPL_NAME "bio_none"
+#define BDEVT_NAME "bdevt"
+#define BDEVT_CTL_NAME "bdevt_ctl"
 
 /*******************************************************************************
  * Static functions definition.
@@ -155,12 +147,12 @@ static struct bio_list split_bio_sectors(struct bio *bio)
 	return bl;
 }
 
-static void smpl_queue_bio(struct request_queue *q, struct bio *bio)
+static void bdevt_queue_bio(struct request_queue *q, struct bio *bio)
 {
 	struct bio *clone, *bio2;
 	struct bio_list bl;
 
-	/* struct smpl_dev *sdev = q->queuedata; */
+	/* struct bdevt_dev *mdev = q->queuedata; */
 	print_bio(bio);
 
 retry0:
@@ -189,17 +181,17 @@ retry0:
 	bio_endio(bio, 0);
 }
 
-static int smpl_open(struct block_device *bdev, fmode_t mode)
+static int bdevt_open(struct block_device *bdev, fmode_t mode)
 {
 	return 0;
 }
 
-static void smpl_release(struct gendisk *gd, fmode_t mode)
+static void bdevt_release(struct gendisk *gd, fmode_t mode)
 {
 	/* do nothing */
 }
 
-static int smpl_ioctl(struct block_device *bdev, fmode_t mode,
+static int bdevt_ioctl(struct block_device *bdev, fmode_t mode,
 			unsigned int cmd, unsigned long arg)
 {
 	return -ENOTTY;
@@ -209,55 +201,55 @@ static int smpl_ioctl(struct block_device *bdev, fmode_t mode,
  * Static variables definition.
  *******************************************************************************/
 
-static struct block_device_operations smpl_devops_ = {
+static struct block_device_operations bdevt_devops_ = {
 	.owner		 = THIS_MODULE,
-	.open		 = smpl_open,
-	.release	 = smpl_release,
-	.ioctl		 = smpl_ioctl
+	.open		 = bdevt_open,
+	.release	 = bdevt_release,
+	.ioctl		 = bdevt_ioctl
 };
 
 /*******************************************************************************
  * Static functions definition.
  *******************************************************************************/
 
-static void del_dev(struct smpl_dev *sdev)
+static void del_dev(struct bdevt_dev *mdev)
 {
-	list_del_init(&sdev->list);
+	list_del_init(&mdev->list);
 
-	del_gendisk(sdev->disk);
-	blk_cleanup_queue(sdev->q);
-	put_disk(sdev->disk);
-	kfree(sdev);
+	del_gendisk(mdev->disk);
+	blk_cleanup_queue(mdev->q);
+	put_disk(mdev->disk);
+	kfree(mdev);
 }
 
 static bool add_dev(u64 size_lb)
 {
-	struct smpl_dev *sdev;
+	struct bdevt_dev *mdev;
 	struct gendisk *disk;
 	struct request_queue *q;
 
-	sdev = kzalloc(sizeof(*sdev), GFP_KERNEL);
-	if (!sdev)
+	mdev = kzalloc(sizeof(*mdev), GFP_KERNEL);
+	if (!mdev)
 		return -ENOMEM;
 
-	INIT_LIST_HEAD(&sdev->list);
+	INIT_LIST_HEAD(&mdev->list);
 
-	q = sdev->q = blk_alloc_queue(GFP_KERNEL);
+	q = mdev->q = blk_alloc_queue(GFP_KERNEL);
 	if (!q) {
-		LOGe("sdev->q init failed.\n");
+		LOGe("mdev->q init failed.\n");
 		goto error0;
 	}
-	q->queuedata = sdev;
-	blk_queue_make_request(q, smpl_queue_bio);
-	disk = sdev->disk = alloc_disk(1);
+	q->queuedata = mdev;
+	blk_queue_make_request(q, bdevt_queue_bio);
+	disk = mdev->disk = alloc_disk(1);
 	if (!disk) {
-		LOGe("sdev->disk alloc failed.\n");
+		LOGe("mdev->disk alloc failed.\n");
 		goto error1;
 	}
 
 	mutex_lock(&lock_);
-	list_add_tail(&sdev->list, &dev_list_);
-	sdev->index = dev_indexes_++;
+	list_add_tail(&mdev->list, &dev_list_);
+	mdev->index = dev_indexes_++;
 	mutex_unlock(&lock_);
 
 	blk_queue_logical_block_size(q, LBS);
@@ -273,18 +265,18 @@ static bool add_dev(u64 size_lb)
 
 	disk->flags |= GENHD_FL_EXT_DEVT;
 	disk->major = major_;
-	disk->first_minor = sdev->index;
-	disk->fops = &smpl_devops_;
-	disk->private_data = sdev;
+	disk->first_minor = mdev->index;
+	disk->fops = &bdevt_devops_;
+	disk->private_data = mdev;
 	disk->queue = q;
-	sprintf(disk->disk_name, "%s%u", SMPL_NAME, sdev->index);
+	sprintf(disk->disk_name, "%s%u", BDEVT_NAME, mdev->index);
 	add_disk(disk);
 	return true;
 
 error1:
-	blk_cleanup_queue(sdev->q);
+	blk_cleanup_queue(mdev->q);
 error0:
-	kfree(sdev);
+	kfree(mdev);
 	return false;
 }
 
@@ -292,47 +284,17 @@ static void exit_all_devices(void)
 {
 	mutex_lock(&lock_);
 	while (!list_empty(&dev_list_)) {
-		struct smpl_dev *sdev =
-			list_entry(dev_list_.next, struct smpl_dev, list);
-		del_dev(sdev);
+		struct bdevt_dev *mdev =
+			list_entry(dev_list_.next, struct bdevt_dev, list);
+		del_dev(mdev);
 	}
 	mutex_unlock(&lock_);
-}
-
-static bool init_all_devices(void)
-{
-	const char *p = dev_sizes_;
-	const char *q;
-	u64 size = 0;
-	unsigned int c = 0;
-	while (parse_u64(p, &q, &size)) {
-		if (!add_dev(size / LBS))
-			goto error;
-		p = q;
-		c++;
-	}
-	if (c == 0) {
-		LOGe("Size not specified.");
-		goto error;
-	}
-	return true;
-error:
-	exit_all_devices();
-	return false;
 }
 
 static bool is_valid_params(void)
 {
 	if (pbs_ != 512 && pbs_ != 4096) {
 		LOGe("pbs must be 512 or 4096.\n");
-		return false;
-	}
-	if (nr_dev_ < 1) {
-		LOGe("nr_dev must be positive integer\n.");
-		return false;
-	}
-	if (hw_queue_depth_ < 1 || hw_queue_depth_ > 64) {
-		LOGe("hw_queue_depth must be in [1, 64].");
 		return false;
 	}
 
@@ -345,45 +307,169 @@ static void init_globals(void)
 }
 
 /*******************************************************************************
+ * Ioctl functions.
+ *******************************************************************************/
+
+static int ioctl_start_dev(struct bdevt_ctl *ctl)
+{
+	/* QQQ */
+	return -EFAULT;
+}
+static int ioctl_get_major(struct bdevt_ctl *ctl)
+{
+	/* QQQ */
+	return -EFAULT;
+}
+static int ioctl_num_of_dev(struct bdevt_ctl *ctl)
+{
+	/* QQQ */
+	return -EFAULT;
+}
+
+static int dispatch_ioctl(struct bdevt_ctl *ctl)
+{
+	size_t i;
+	struct {
+		int id;
+		int (*handler)(struct bdevt_ctl *ctl);
+	} tbl[] = {
+		{BDEVT_IOCTL_START_DEV, ioctl_start_dev},
+		{BDEVT_IOCTL_GET_MAJOR, ioctl_get_major},
+		{BDEVT_IOCTL_NUM_OF_DEV, ioctl_num_of_dev},
+	};
+
+	for (i = 0; i < sizeof(tbl); i++) {
+		if (ctl->command == tbl[i].id)
+			return tbl[i].handler(ctl);
+	}
+	LOGe("dispatch_ioctl: command %d is not supported.\n",
+		ctl->command);
+	return -ENOTTY;
+}
+
+static struct bdevt_ctl *bdevt_get_ctl(void __user *userctl, gfp_t gfp_mask)
+{
+	struct bdevt_ctl *ctl;
+
+	ctl = kzalloc(sizeof(*ctl), gfp_mask);
+	if (!ctl) {
+		LOGe("memory allocation for bdevt_ctl error.\n");
+		goto error0;
+	}
+
+	if (copy_from_user(ctl, userctl, sizeof(*ctl))) {
+		LOGe("copy_from_user failed.\n");
+		goto error1;
+	}
+
+	return ctl;
+
+error1:
+	kfree(ctl);
+error0:
+	return NULL;
+}
+
+static bool bdevt_put_ctl(void __user *userctl, struct bdevt_ctl *ctl)
+{
+	bool ret = true;
+
+	if (copy_to_user(userctl, ctl, sizeof(*ctl)))
+		ret = false;
+
+	kfree(ctl);
+	return ret;
+}
+
+static long bdevt_ctl_ioctl(struct file *file, unsigned int command, unsigned long u)
+{
+	int ret;
+	struct bdevt_ctl *ctl;
+	struct bdevt_ctl __user *user = (struct bdevt_ctl __user *)u;
+
+	if (command != BDEVT_IOCTL_CMD)
+		return -EFAULT;
+
+	ctl = bdevt_get_ctl(user, GFP_KERNEL);
+	if (!ctl)
+		return -EFAULT;
+
+	ret = dispatch_ioctl(ctl);
+
+	if (!bdevt_put_ctl(user, ctl))
+		return -EFAULT;
+
+	return ret;
+}
+
+#ifdef CONFIG_COMPAT
+static long bdevt_ctl_compat_ioctl(struct file *file, unsigned int command, unsigned long u)
+{
+	return bdevt_ctl_ioctl(file, command, (unsigned long)compat_ptr(u));
+}
+#endif
+
+static const struct file_operations ctl_fops_ = {
+	.open = nonseekable_open,
+	.unlocked_ioctl = bdevt_ctl_ioctl,
+	.compat_ioctl = bdevt_ctl_compat_ioctl,
+	.owner = THIS_MODULE,
+};
+
+static struct miscdevice bdevt_misc_ = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name  = BDEVT_NAME,
+	.nodename = BDEVT_CTL_NAME,
+	.fops = &ctl_fops_,
+};
+
+/*******************************************************************************
  * Init/exit functions definition.
  *******************************************************************************/
 
-static int __init smpl_init(void)
+static int __init bdevt_init(void)
 {
-	LOGi("%s module init.\n", SMPL_NAME);
+	LOGi("%s module init.\n", BDEVT_NAME);
 	LOGi("build date: " BUILD_DATE "\n");
 
 	if (!is_valid_params()) goto error0;
 	init_globals();
 
 	/* Register a block device module. */
-	major_ = register_blkdev(0, SMPL_NAME);
+	major_ = register_blkdev(0, BDEVT_NAME);
 	if (major_ <= 0) {
 		LOGe("unable to get major device number.\n");
 		goto error0;
 	}
 
-	if (!init_all_devices()) goto error1;
-	return 0;
+	if (misc_register(&bdevt_misc_) < 0) {
+		LOGe("unable to register control device.\n");
+		goto error1;
+	}
 
+	return 0;
+#if 0
+error2:
+	misc_deregister(&bdevt_misc_);
+#endif
 error1:
-	exit_all_devices();
-	unregister_blkdev(major_, SMPL_NAME);
+	unregister_blkdev(major_, BDEVT_NAME);
 	return -ENOMEM;
 error0:
 	return -EBUSY;
 }
 
-static void __exit smpl_exit(void)
+static void __exit bdevt_exit(void)
 {
+	misc_deregister(&bdevt_misc_);
 	exit_all_devices();
-	unregister_blkdev(major_, SMPL_NAME);
-	LOGi("%s module exit.\n", SMPL_NAME);
+	unregister_blkdev(major_, BDEVT_NAME);
+	LOGi("%s module exit.\n", BDEVT_NAME);
 }
 
-module_init(smpl_init);
-module_exit(smpl_exit);
+module_init(bdevt_init);
+module_exit(bdevt_exit);
 MODULE_LICENSE("Dual BSD/GPL");
-MODULE_DESCRIPTION("memory block device using bio");
-MODULE_ALIAS(SMPL_NAME);
-/* MODULE_ALIAS_BLOCKDEV_MAJOR(SMPL_MAJOR); */
+MODULE_DESCRIPTION("a memory block device driver for test");
+MODULE_ALIAS(BDEVT_NAME);
+/* MODULE_ALIAS_BLOCKDEV_MAJOR(BDEVT_MAJOR); */
